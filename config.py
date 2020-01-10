464949e4735e1d3222896b40a2399228e0ede8e3
Piyush Bagad <piyush@wadhwaniai.org>
2020-01-10 15:49:15 +0530

    [MAJOR] Adds config files and a reader of those configs

---
+++ config.py
@@ -0,0 +1,42 @@
+from os import makedirs
+from os.path import join, basename, expanduser
+
+from utils.constants import DATA_DIR
+from utils.io import read_yml
+
+
+USER_TO_HOME = {
+    'piyushb': '/home/users/{}/projects/correlation-GAN',
+}
+
+class Config(object):
+    """docstring for Config"""
+    def __init__(self, version):
+        super(Config, self).__init__()
+        self.version = version
+
+        username = expanduser('~').split('/')[-1]
+        self.paths = self.define_paths(username)
+
+        config_path = join(self.paths['HOME'], 'configs', version)
+        self.__dict__.update(read_yml(config_path))
+
+    def define_paths(self, username):
+        HOME = USER_TO_HOME[username].format(username)
+        COMMON_DIR = join(HOME, 'common')
+        OUT_DIR = '/scratch/users/{}/correlation-GAN/'.format(username)
+        CKPT_DIR = join(OUT_DIR, 'checkpoints')
+        LOG_DIR = join(OUT_DIR, 'logs')
+        CONFIG_DIR = join(OUT_DIR, 'configs')
+        DATA_DIR = '/home/users/piyushb/data/'
+
+        makedirs(CONFIG_DIR, exist_ok=True)
+        keys = ['HOME', 'CKPT_DIR', 'LOG_DIR', 'CONFIG_DIR', 'COMMON_DIR', 'DATA_DIR']
+        dir_dict = {}
+        for key in keys:
+            dir_dict[key] = eval(key)
+        return dir_dict
+
+if __name__ == '__main__':
+    config = Config('default.yml')
+    import ipdb; ipdb.set_trace()
