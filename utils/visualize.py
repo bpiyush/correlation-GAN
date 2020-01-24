b883f7b1b599ea33c730c774c550f82db51a7551
Piyush Bagad <piyush@wadhwaniai.org>
2020-01-24 15:06:43 +0530

    [] Script for visualisation functions

---
+++ utils/visualize.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def fig2data(fig):
+    """
+    brief Convert a Matplotlib figure to a 4D numpy array with RGBA
+    channels and return it
+    @param fig: a matplotlib figure
+    @return a numpy 3D array of RGBA values
+    """
+    # draw the renderer
+    fig.canvas.draw()
+
+    # Get the RGBA buffer from the figure
+    w, h = fig.canvas.get_width_height()
+    buf = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
+    buf.shape = (w, h, 4)
+
+    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+    buf = np.roll(buf, 3, axis=2)
+    return buf.reshape(h, w, 4)
+
+
+def fig2im(fig):
+    """
+    convert figure to ndarray
+    """
+    img_data = fig2data(fig).astype(np.int32)
+    plt.close()
+    return img_data[:, :, :3] / 255.
+
+
+def plot_original_vs_generated(original_data, generated_data):
+    """Predicted vs ground truth weight"""
+
+    assert len(original_data.shape) == 2 and len(generated_data.shape) == 2
+
+    fig, ax = plt.subplots(1, figsize=(5, 5))
+    ax.grid()
+    ax.scatter(original_data[:, 0], original_data[:, 1], label='Original')
+    ax.scatter(generated_data[:, 0], generated_data[:, 1], label='Generated')
+    ax.legend()
+
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+
+    return fig2im(fig)
